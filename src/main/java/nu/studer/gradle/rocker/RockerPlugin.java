3c834c6d5c0b3816a107596bfc057bef06343648
etiennestuder <etienne@studer.nu>
2017-03-25 23:57:03 +0100

    All to configure rocker version and apply it to all rocker dependencies

--- src/main/java/nu/studer/gradle/rocker/RockerPlugin.java
+++ src/main/java/nu/studer/gradle/rocker/RockerPlugin.java
@@ -6,6 +6,9 @@ import org.gradle.api.NamedDomainObjectFactory;
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
 import org.gradle.api.artifacts.Configuration;
+import org.gradle.api.artifacts.DependencyResolveDetails;
+import org.gradle.api.artifacts.ModuleVersionSelector;
+import org.gradle.api.artifacts.ResolutionStrategy;
 import org.gradle.api.plugins.JavaBasePlugin;
 import org.gradle.api.plugins.JavaPluginConvention;
 import org.gradle.api.tasks.SourceSet;
@@ -16,12 +19,21 @@ import org.slf4j.LoggerFactory;
 @SuppressWarnings("unused")
 public class RockerPlugin implements Plugin<Project> {
 
+    private static final String ROCKER_VERSION_PROPERTY = "rockerVersion";
+    private static final String DEFAULT_ROCKER_VERSION = "0.16.0";
+
     private static final Logger LOGGER = LoggerFactory.getLogger(RockerPlugin.class);
 
     @Override
     public void apply(final Project project) {
         // apply Java base plugin, making it possible to also use the rocker plugin for Android builds
         project.getPlugins().apply(JavaBasePlugin.class);
+
+        // allow to configure the rocker version via extension property
+        project.getExtensions().getExtraProperties().set(ROCKER_VERSION_PROPERTY, DEFAULT_ROCKER_VERSION);
+
+        // use the configured rocker version on all rocker dependencies
+        enforceRockerVersion(project);
 
         // add rocker DSL extension
         NamedDomainObjectContainer<RockerConfig> container = project.container(RockerConfig.class, new NamedDomainObjectFactory<RockerConfig>() {
@@ -46,12 +58,12 @@ public class RockerPlugin implements Plugin<Project> {
                 rocker.config = config;
                 rocker.rockerCompilerRuntime = configuration;
 
-                // wire task dependencies such that rocker creates the sources before the corresponding compile task compiles them
+                // wire task dependencies such that the rocker task creates the sources before the corresponding Java compile task compiles them
                 SourceSetContainer sourceSets = project.getConvention().getPlugin(JavaPluginConvention.class).getSourceSets();
                 SourceSet sourceSet = sourceSets.findByName(config.name);
                 if (sourceSet != null) {
                     project.getTasks().getByName(sourceSet.getCompileJavaTaskName()).dependsOn(rocker);
-                    project.getDependencies().add(sourceSet.getCompileConfigurationName(), "com.fizzed:rocker-runtime:0.16.0");
+                    project.getDependencies().add(sourceSet.getCompileConfigurationName(), "com.fizzed:rocker-runtime");
                 }
             }
         });
@@ -60,9 +72,31 @@ public class RockerPlugin implements Plugin<Project> {
     private Configuration createRockerCompilerRuntimeConfiguration(Project project) {
         Configuration rockerCompilerRuntime = project.getConfigurations().create("rockerCompiler");
         rockerCompilerRuntime.setDescription("The classpath used to invoke the Rocker template engine. Add your additional dependencies here.");
-        project.getDependencies().add(rockerCompilerRuntime.getName(), "com.fizzed:rocker-compiler:0.16.0");
+        project.getDependencies().add(rockerCompilerRuntime.getName(), "com.fizzed:rocker-compiler");
         project.getDependencies().add(rockerCompilerRuntime.getName(), "org.slf4j:slf4j-simple:1.7.23");
         return rockerCompilerRuntime;
+    }
+
+    private void enforceRockerVersion(final Project project) {
+        project.getConfigurations().all(new Action<Configuration>() {
+            @Override
+            public void execute(Configuration configuration) {
+                configuration.resolutionStrategy(new Action<ResolutionStrategy>() {
+                    @Override
+                    public void execute(ResolutionStrategy resolutionStrategy) {
+                        resolutionStrategy.eachDependency(new Action<DependencyResolveDetails>() {
+                            @Override
+                            public void execute(DependencyResolveDetails details) {
+                                ModuleVersionSelector requested = details.getRequested();
+                                if (requested.getGroup().equals("com.fizzed") && requested.getName().startsWith("rocker-")) {
+                                    details.useVersion(project.getExtensions().getExtraProperties().get(ROCKER_VERSION_PROPERTY).toString());
+                                }
+                            }
+                        });
+                    }
+                });
+            }
+        });
     }
 
     private static String capitalize(String s) {
