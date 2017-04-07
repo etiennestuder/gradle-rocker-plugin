b7350227117e4245af67eb7b940446dc16400a40
etiennestuder <etienne@studer.nu>
2017-04-07 06:11:21 +0200

    Make example work

--- example/src/main/java/Main.java
+++ example/src/main/java/Main.java
@@ -1,7 +1,10 @@
+import com.fizzed.rocker.RockerModel;
+import com.fizzed.rocker.runtime.StringBuilderOutput;
+
 public class Main {
 
     public static void main(String[] args) {
-        RockerModel model = Hello.template("Switzerland")
+        RockerModel model = Hello.template("Switzerland");
         StringBuilderOutput output = model.render(StringBuilderOutput.FACTORY);
         System.out.println(output.getBuffer().toString());
     }
