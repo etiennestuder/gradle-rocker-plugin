dac536089d6b59ff8e03b1118192599367767b10
etiennestuder <etienne@studer.nu>
2017-04-15 17:33:12 +0200

    Do not include classDir property in uptodate check (mark as @Internal)
    The classDir value has no effect on the generated sources. It is only relevant in that the generated rocker-compiler.conf file is created in that directory and the given conf file contains the classDir entry, too (all this is only relevant when optimize=false).

--- src/main/java/nu/studer/gradle/rocker/RockerConfig.java
+++ src/main/java/nu/studer/gradle/rocker/RockerConfig.java
@@ -4,6 +4,7 @@ import org.gradle.api.Project;
 import org.gradle.api.plugins.JavaPluginConvention;
 import org.gradle.api.tasks.Input;
 import org.gradle.api.tasks.InputDirectory;
+import org.gradle.api.tasks.Internal;
 import org.gradle.api.tasks.Optional;
 import org.gradle.api.tasks.OutputDirectory;
 import org.gradle.api.tasks.PathSensitive;
@@ -129,8 +130,8 @@ public class RockerConfig {
         }
     }
 
-    // do not annotate as @OutputDirectory!
-    @Input
+    // do not include in uptodate check, not as input nor as output
+    @Internal
     File getClassDir() {
         return classDir;
     }
