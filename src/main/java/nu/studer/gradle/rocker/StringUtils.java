63145ed8237d27542c55c5834ec19d0df58f1ffe
etiennestuder <etienne@studer.nu>
2017-05-19 10:04:46 +0200

    Reduce visibility

--- src/main/java/nu/studer/gradle/rocker/StringUtils.java
+++ src/main/java/nu/studer/gradle/rocker/StringUtils.java
@@ -1,8 +1,12 @@
 package nu.studer.gradle.rocker;
 
-public abstract class StringUtils {
+final class StringUtils {
 
-    public static String capitalize(String s) {
+    private StringUtils() {
+    }
+
+    static String capitalize(String s) {
         return s == null || s.isEmpty() ? s : Character.toUpperCase(s.charAt(0)) + s.substring(1);
     }
+
 }
