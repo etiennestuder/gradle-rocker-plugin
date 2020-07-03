d724f53ab2c612518e795e85a033303acc985aab
Etienne Studer <etienne@studer.nu>
2020-07-03 18:16:27 +0200

    Clean up Kotlin DSL example

--- example/kotlin/build.gradle.kts
+++ example/kotlin/build.gradle.kts
@@ -1,7 +1,5 @@
-import nu.studer.gradle.rocker.RockerConfig
-
 plugins {
-    id("nu.studer.rocker") version "2.1"
+    id("nu.studer.rocker") version "2.1.1"
     id("java")
 }
 
@@ -13,8 +11,8 @@ val rockerVersion by extra("1.3.0")
 
 rocker {
     create("main") {
-        templateDir = file("src/rocker")
-        outputDir = file("src/generated/rocker")
-        isOptimize = true // optional
+        optimize.set(true) // optional
+        templateDir.set(file("src/rocker"))
+        outputDir.set(file("src/generated/rocker"))
     }
 }
