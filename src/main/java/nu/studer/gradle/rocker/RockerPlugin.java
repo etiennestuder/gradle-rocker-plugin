e077a9b8002200c69a90c4a8adcf4a297bcd47bf
Etienne Studer <etienne@studer.nu>
2020-08-28 18:33:43 +0200

    Make Gradle 6.1 the minimum accepted version

--- src/main/java/nu/studer/gradle/rocker/RockerPlugin.java
+++ src/main/java/nu/studer/gradle/rocker/RockerPlugin.java
@@ -23,8 +23,8 @@ public class RockerPlugin implements Plugin<Project> {
     @Override
     public void apply(Project project) {
         // abort if old Gradle version is not supported
-        if (GradleVersion.current().getBaseVersion().compareTo(GradleVersion.version("6.0")) < 0) {
-            throw new IllegalStateException("This version of the rocker plugin is not compatible with Gradle < 6.0");
+        if (GradleVersion.current().getBaseVersion().compareTo(GradleVersion.version("6.1")) < 0) {
+            throw new IllegalStateException("This version of the rocker plugin is not compatible with Gradle < 6.1");
         }
 
         // apply Java base plugin, making it possible to also use the rocker plugin for Android builds
